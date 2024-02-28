35951b3f98be46ab26573f2605fbcaee7e63585a
sankarravada0204 <sankarravada0204@gmail.com>
2024-02-28 17:33:19 +0530

    playwright java project

---
+++ src/main/java/io/github/tahanima/config/Configuration.java
@@ -0,0 +1,31 @@
+package io.github.tahanima.config;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.LoadPolicy;
+import org.aeonbits.owner.Config.Sources;
+
+/**
+ * @author tahanima
+ */
+@LoadPolicy(Config.LoadType.MERGE)
+@Sources({"system:properties", "classpath:config.properties", "classpath:allure.properties"})
+public interface Configuration extends Config {
+
+    String browser();
+
+    @Key("base.url")
+    String baseUrl();
+
+    boolean headless();
+
+    @Key("slow.motion")
+    int slowMotion();
+
+    int timeout();
+
+    @Key("base.test.data.path")
+    String baseTestDataPath();
+
+    @Key("allure.results.directory")
+    String allureResultsDir();
+}
---
+++ src/test/java/io/github/tahanima/e2e/ProductsTest.java
@@ -0,0 +1,52 @@
+package io.github.tahanima.e2e;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import io.github.artsok.ParameterizedRepeatedIfExceptionsTest;
+import io.github.tahanima.annotation.DataSource;
+import io.github.tahanima.annotation.Smoke;
+import io.github.tahanima.dto.ProductsDto;
+import io.github.tahanima.ui.page.LoginPage;
+import io.qameta.allure.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * @author sankar
+ */
+@Feature("Products Test")
+public class ProductsTest extends BaseTest {
+
+    private static final String PATH = "products.csv";
+
+    @BeforeEach
+    public void createBrowserContextAndPageAndLoginPageInstances() {
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+
+        loginPage = createInstance(LoginPage.class);
+    }
+
+    @Attachment(value = "Failed Test Case Screenshot", type = "image/png")
+    protected byte[] captureScreenshotOnFailure() {
+        return loginPage.captureScreenshot();
+    }
+
+    @AfterEach
+    public void closeBrowserContextSession() {
+        browserContext.close();
+    }
+
+    @Smoke
+    @Story("Logging out from Products page should redirect to Login page")
+    @Owner("Ravada Sankar")
+    @Description("Test that verifies user gets redirected to 'Login' page after logging out")
+    @ParameterizedRepeatedIfExceptionsTest
+    @DataSource(id = "TC-1", fileName = PATH, clazz = ProductsDto.class)
+    public void testSuccessfulLogout(final ProductsDto data) {
+        loginPage.loginAs(data.getUsername(), data.getPassword()).clickOnLogout();
+
+        assertThat(page).hasURL(data.getUrl());
+    }
+}
